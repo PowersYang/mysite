acac94af6a310689effaa8ec536f962b2b98dccf
yangwangqiang <yangwangqiang@ejie365.com>
2018-07-24 17:48:23 +0800

    2018.7.24 12:00

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'polls/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -107,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
